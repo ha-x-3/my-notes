796376e55553f03682faacf5d5252e73ec52b576
Brittney Epperson <beppers87@hotmail.com>
2024-12-03 16:32:52 -0600

    Added backend Spring Boot files

---
+++ backend/my-notes/src/test/java/com/launchcodeconnect/my_notes/MyNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcodeconnect.my_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
